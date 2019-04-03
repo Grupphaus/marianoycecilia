a2e475cc22520417d000b30a9f7c499d712a96bb
Federico Pellegrini <fellegrini@gmail.com>
2019-04-03 05:35:26 -0300

    modificacion de estilos, piezas de texto

--- marianocecila.js
+++ marianocecila.js
@@ -1,3 +1,15 @@
 window.onload = () => {
-    document.getElementById('loader').classList.add('bye')
+    document.getElementById('loader').classList.add('bye');
+    document.getElementById('main').classList.add('hello');
+    document.getElementById('footer').classList.add('hello');
 }
+
+//  function initMap() {
+//      // The location of Uluru
+//      var uluru = {lat: -34.6414974, lng: -58.5395091};
+//      // The map, centered at Uluru
+//      var map = new google.maps.Map(
+//          document.getElementById('map'), {zoom: 100, center: uluru});
+//      // The marker, positioned at Uluru
+//      var marker = new google.maps.Marker({position: uluru, map: map});
+//    }
