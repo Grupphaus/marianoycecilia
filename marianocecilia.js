dbd9d462ca1336c89dc2742adc72c217ef89dc91
Federico Pellegrini <fellegrini@gmail.com>
2019-04-04 04:42:47 -0300

    last test of the day

--- marianocecilia.js
+++ marianocecilia.js
@@ -1,11 +1,8 @@
-document.onreadystatechange = () => {
+window.onload = () => {
     setTimeout(function(){
         // This hides the address bar:
         window.scrollTo(0, 1);
     }, 0);
-}
-
-window.onload = () => {
     setTimeout(() => {
         document.getElementById('loader').classList.add('bye');
         document.getElementById('main').classList.add('hello');
