71bda5906ce6ac54d5460422f9ddec2a38b5de7a
Federico Pellegrini <fellegrini@gmail.com>
2019-04-04 03:17:04 -0300

    major bug fixes regarding media queries

--- marianocecilia.js
+++ marianocecilia.js
@@ -2,6 +2,11 @@ window.onload = () => {
     setTimeout(() => {
         document.getElementById('loader').classList.add('bye');
         document.getElementById('main').classList.add('hello');
+        document.getElementById('footer').classList.add('hello');
     }, 2000);
-    setTimeout(() => document.getElementById('imagen').classList.add('start-slide'), 3000);
+    setTimeout(() => {
+        document.getElementById('imagen').classList.add('start-slide');
+        var elem = document.querySelector('#loader');
+        elem.parentNode.removeChild(elem);
+    }, 3000);
 }
