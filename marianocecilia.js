4235e99a49d41065fa508f6300670226b57a5517
Federico Pellegrini <fellegrini@gmail.com>
2019-04-04 02:23:44 -0300

    tyding up, optimizing loadtimes

--- marianocecilia.js
+++ marianocecilia.js
@@ -2,7 +2,6 @@ window.onload = () => {
     setTimeout(() => {
         document.getElementById('loader').classList.add('bye');
         document.getElementById('main').classList.add('hello');
-        document.getElementById('footer').classList.add('hello');
     }, 2000);
     setTimeout(() => document.getElementById('imagen').classList.add('start-slide'), 3000);
 }
