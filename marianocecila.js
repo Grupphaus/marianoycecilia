acd8f64cc52e4c5428caf99826468d336c0c4d7b
Federico Pellegrini <fellegrini@gmail.com>
2019-04-04 01:03:46 -0300

    bug fixing #1

--- marianocecila.js
+++ marianocecila.js
@@ -4,14 +4,5 @@ window.onload = () => {
         document.getElementById('main').classList.add('hello');
         document.getElementById('footer').classList.add('hello');
     }, 2000);
+    setTimeout(() => document.getElementById('imagen').classList.add('start-slide'), 3000);
 }
-
-//  function initMap() {
-//      // The location of Uluru
-//      var uluru = {lat: -34.6414974, lng: -58.5395091};
-//      // The map, centered at Uluru
-//      var map = new google.maps.Map(
-//          document.getElementById('map'), {zoom: 100, center: uluru});
-//      // The marker, positioned at Uluru
-//      var marker = new google.maps.Marker({position: uluru, map: map});
-//    }
