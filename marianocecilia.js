52a3a7a87ebe2fae5fa1b6d8ace924196da21f3a
Federico Pellegrini <fellegrini@gmail.com>
2019-04-13 04:01:18 -0300

    ch-ch-ch-ch-changes

--- marianocecilia.js
+++ marianocecilia.js
@@ -1,11 +1,61 @@
-window.onload = () => {
-    setTimeout(() => {
-        document.getElementById('loader').classList.add('bye');
-        document.getElementById('main').classList.add('hello');
-        document.getElementById('footer').classList.add('hello');
-    }, 3000);
-    setTimeout(() => {
-        var elem = document.querySelector('#loader');
-        elem.parentNode.removeChild(elem);
-    }, 6000);
-}
+(function () {
+
+        function zeros(i) {
+                if (i < 10) {
+                    i = "0" + i;
+                }
+                return i;
+                }
+
+        let deadline = ('November 09 2019 20:15:00 GMT-3');
+        let daysSpan = clock.querySelector('.days');
+        let hoursSpan = clock.querySelector('.hours');
+        let minutesSpan = clock.querySelector('.minutes');
+        let secondsSpan = clock.querySelector('.seconds');
+
+        function getTimeRemaining(deadline) {
+                let t = Date.parse(deadline) - Date.parse(new Date());
+                let seconds = zeros(Math.floor((t / 1000) % 60));
+                let minutes = zeros(Math.floor((t / 1000 / 60) % 60));
+                let hours = zeros(Math.floor((t / (1000 * 60 * 60)) % 24));
+                let days = Math.floor(t / (1000 * 60 * 60 * 24));
+                return {
+                        'total': t,
+                        'days': days,
+                        'hours': hours,
+                        'minutes': minutes,
+                        'seconds': seconds
+                };
+        }
+
+        function initializeClock(id, deadline) {
+                var clock = document.getElementById('clock');
+                var timeinterval = setInterval(function () {
+                        var t = getTimeRemaining(deadline);
+                        clock.innerHTML = t.days + ' DÍAS ' +
+                                t.hours + ' : ' +
+                                t.minutes + ' : ' +
+                                t.seconds;
+                        if (t.total <= 0) {
+                                clearInterval(timeinterval);
+                        }
+                }, 1000);
+        }
+
+        function updateClock() {
+                let t = getTimeRemaining(deadline);
+                daysSpan.innerHTML = t.days;
+                hoursSpan.innerHTML = t.hours;
+                minutesSpan.innerHTML = t.minutes;
+                secondsSpan.innerHTML = t.seconds;
+                if (t.total <= 0) {
+                        clearInterval(timeinterval);
+                }
+        }
+
+        updateClock();
+        let timeinterval = setInterval(updateClock, 1000);
+
+
+        initializeClock('clockdiv', deadline);
+}());
