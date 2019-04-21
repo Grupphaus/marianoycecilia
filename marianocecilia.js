252f8eea5120da01b5bdfce71dec7c117755f867
Federico Pellegrini <fellegrini@gmail.com>
2019-04-21 18:34:47 -0300

    testing for mobile performance

--- marianocecilia.js
+++ marianocecilia.js
@@ -1,21 +1,31 @@
 $('window').ready(() => {
 
+        let vh = window.innerHeight * 0.01;
+        // Then we set the value in the --vh custom property to the root of the document
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+
         $("#loader").animate({
                 opacity: 1
-        })
+        }, 600)
+
+        $('html, body').animate({
+                scrollTop: 0
+        }, 400);    
+
         $(".btn").click(function (e) { 
                 e.preventDefault();
                 
                 $(".loader-main").animate({
                         opacity: 0
-                }, 300, () => {
+                }, 400, () => {
                         $("#loader").slideUp(800, () => {
                                 $("#main").animate({
                                         opacity: 1
                                 }, 500)
                                 $("footer").animate({
                                         opacity: 1
-                                }, 500)        
+                                }, 500)
+                                    
                         });
                 })
         });
@@ -79,7 +89,7 @@ $('window').ready(() => {
 
         $(".loader-text").delay(500).animate({
                 opacity: 1
-        })
+        });
 
         $(".clock").delay(1000).animate({
                 opacity: 1,
@@ -93,47 +103,34 @@ $('window').ready(() => {
                 $('html, body').animate({
                         scrollTop: $("#fiesta-wrapper").offset().top
                     }, 800);
-        })
+        });
 
         $("#button-to-lugar").click(() => {
                 $('html, body').animate({
                         scrollTop: 0
                     }, 800);
-        })
+        });
 
         $("#button-to-fiesta-bot").click(() => {
                 $('html, body').animate({
                         scrollTop: $("#fiesta-wrapper").offset().top
                     }, 800);
-        })
+        });
 
         $("#button-to-regalo").click(() => {
                 $('html, body').animate({
                         scrollTop: $("#regalo-wrapper").offset().top
                     }, 800);
-        })
+        });
 
         $("#button-to-top").click(() => {
                 $('html, body').animate({
                         scrollTop: 0
                     }, 800);
-        })
+        });
 
-        var keys = [32,33,34,35,36,37,38,39,40];
-
-function preventDefault(e) {
-  e = e || window.event;
-  if (e.preventDefault)
-      e.preventDefault();
-  e.returnValue = false;  
-}
-
-function keydown(e) {
-    for (var i = keys.length; i--;) {
-        if (e.keyCode === keys[i]) {
-            preventDefault(e);
-            return;
-        }
-    }
-}
+        window.addEventListener('resize', () => {
+                let vh = $("window").innerHeight * 0.01;
+                document.documentElement.style.setProperty('--vh', `${vh}px`);
+              });
 }); 
